e7d69acd8049678a503e2e52be28464742734441
unknown <yasinabdurakhmanov@gmail.com>
2023-12-13 11:46:53 +0300

    changes

---
+++ openapi.py
@@ -0,0 +1,22 @@
+import openai
+import os
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+messages = [
+    {"role": "system", "content" : ""}
+    ]
+
+while True:
+    content = input("User: ")
+    messages.append({"role": "user", "content": content})
+    
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(f'ChatGPT: {chat_response}')
+    messages.append({"role": "assistant", "content": chat_response})
